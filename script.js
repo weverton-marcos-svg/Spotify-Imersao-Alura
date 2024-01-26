0fffa71f2e850fb596e0ac458f9234da9a44b583
Weverton Marcos <wevertonmarcos.842@gmail.com.br>
2024-01-26 00:12:39 -0300

    Aula 04
    Adicionando interação com javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+const searchInput = document.getElementById('search-input')
+const resultsArtist = document.getElementById('result-artist')
+const resultPlaylist = document.getElementById('result-playlists')
+ 
+function requestApi(searchTerm){
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+function displayResults(result){
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImg = document.getElementById('artist-img');
+
+    console.log(result)
+    result.forEach(element => {
+        
+        artistName.innerHTML = element.name;
+        artistImg.src = element.urlImg;
+    });
+
+    if(result.length == []){
+        artistName.innerHTML = ''
+        artistImg.src = ''
+
+    }
+    
+    resultsArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === ''){
+        resultPlaylist.classList.add('hidden');
+        resultsArtist.classList.remove('hidden');
+        return;
+    }
+
+    requestApi(searchTerm)
+})
